c26c41136931a53f52960032d2b7f714ea03c5ed
Harshithash2 <harshit.chauhan2015@gmail.com>
2026-01-02 15:07:53 +0530

    01-01-2026

--- routes/buyer.js
+++ routes/buyer.js
@@ -3,6 +3,6 @@ import { nearbyStores } from '../controllers/buyerController.js';
 import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
-router.post('/nearbySellar/:productId', verifyToken, nearbyStores);
+router.post('/nearbySellar/:productId', nearbyStores);
 
 export default router;
